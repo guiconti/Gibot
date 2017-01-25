bfb1b3d1ad4837dc7f93a98eae927a185d074384
GuiMac <guibasconti@gmail.com>
2017-01-25 06:20:50 -0200

    Documentação nas validações do Gibimbot

--- app/bot/validation.js
+++ app/bot/validation.js
@@ -1,3 +1,14 @@
+/**
+ * Módulo de validações para o Telegram
+ * @module bot/validation
+ */
+
+/**
+ * Valida se uma requisição no Trello é válida.
+ *
+ * @param {string[]} userRequest - Array com todas as opções da requisição.
+ * @return {boolean} - Retorna true se a ação for válida e false caso contrário.
+ */
 exports.isValidAction = (userRequest) => {
 
     //  Por hora so considera request valido se tiver 3 argumentos ou mais
@@ -5,18 +16,36 @@ exports.isValidAction = (userRequest) => {
 
 };
 
+/**
+ * Valida se uma card do Trello é válida.
+ *
+ * @param {string} card - Nome da card para verificação.
+ * @return {boolean} - Retorna true se a card for válida e false caso contrário.
+ */
 exports.isValidCard = (card) => {
 
     return _.isString(card) && card.trim().length > 0;
 
 };
 
+/**
+ * Valida se uma ação de lista é válida.
+ *
+ * @param {string} action - Nome da ação solicitada.
+ * @return {boolean} - Retorna true se a ação de listagem for válida e false caso contrário.
+ */
 exports.isListAction = (action) => {
 
 return action == 'liste' || action == 'list' || action == 'listar' || action == 'lista';
 
 };
 
+/**
+ * Valida se uma ação de insert é válida.
+ *
+ * @param {string} action - Nome da ação solicitada.
+ * @return {boolean} - Retorna true se a ação de listagem for válida e false caso contrário.
+ */
 exports.isInsertAction = (action) => {
 
     return action == 'insert' || action == 'inserir' || action == 'insere' || action == 'insira';
