1243bffcf3ff8bec572c9a4b893f415f53474af2
Guilherme Conti <guibasconti@gmail.com>
2017-10-05 00:31:40 -0300

    Remove gmail module. Closes #10

--- server/core/router.js
+++ server/core/router.js
@@ -32,20 +32,13 @@ const router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({extended: true}));
 
-//  Trello APIs
-router.post('/trello/:boardName/:listName/insert', controllers.trello.insertCard);
-router.get('/trello/:boardName/:listName/list', controllers.trello.showList);
-
 //  Telegram APIs
 router.post('/telegram/message/owner', controllers.telegram.sendMessageToOwner);
 router.post('/telegram/message/chat', controllers.telegram.sendMessageToChat);
 router.post('/telegram/photo/owner', upload.single('image'), controllers.telegram.sendPhotoToOwner);
 router.post('/telegram/photo/chat', upload.single('image'), controllers.telegram.sendPhotoToChat);
 
-/*  Gmail APIs  */
-router.get('/gmail/list', controllers.gmail.listEvents);
-
-/*  Rag APIs    */
+/*  Rag APIs   */
 router.get('/ragnarok/mvp/list', controllers.ragnarok.listMvpTimer);
 router.post('/ragnarok/mvp/insert', controllers.ragnarok.insertMvpTimer);
 
