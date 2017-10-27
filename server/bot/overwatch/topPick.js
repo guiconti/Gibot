3f798fc92ef8140d73f486c6096b1a86021d9e2d
guiconti <guibasconti@gmail.com>
2017-10-27 04:03:13 +0000

    Top picked feature implemented

--- server/bot/overwatch/topPick.js
+++ server/bot/overwatch/topPick.js
@@ -4,15 +4,33 @@
 */
 
 const bot = require('../../core/gibimbot');
+const logger = require('../../../tools/logger');
 const request = require('request');
 const constants = require('../../utils/constants');
 
 module.exports = (msg, match) => {
   let chatId = msg.chat.id;
-  let url = constants.url.OVERWATCH.PREFIX + constants.url.OVERWATCH.TOP_PICK;
-  request.get({url: url}, (err, httpResponse, body) => {
-    if (err)
+  let url = constants.url.overwatch.PREFIX + constants.url.overwatch.TOP_PICK;
+  let headers = constants.url.overwatch.HEADERS;
+  request.get({url: url, headers: headers}, (err, httpResponse, body) => {
+    if (err){
+      logger.error(err);
       return bot.sendMessage(chatId, constants.message.error.TOP_PICK_API);
+    }
+      try {
+        let response = JSON.parse(body);
+        let userResponse = response.msg;
+        if (Array.isArray(response.msg)){
+          userResponse = `Here is the top picked heroes in the last month\n`;
+          response.msg.forEach((hero) => {
+            userResponse += `\n${hero.hero} - ${hero.pick_rate}`;
+          });
+        }
+        return bot.sendMessage(chatId, userResponse);
+      } catch (err) {
+        logger.error(err);
+        return bot.sendMessage(chatId, constants.message.error.TOP_PICK_API);
+      }
     return bot.sendMessage(chatId, body);
   });
 };
