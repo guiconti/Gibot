1d6c96b2ce84cf7fc38b1d753dceb2a45431b190
leonardo a <leonardo.akato@gmail.com>
2017-12-18 22:16:09 -0200

    semanal fag

--- server/bot/overwatch/topPick.js
+++ server/bot/overwatch/topPick.js
@@ -21,7 +21,7 @@ module.exports = (msg, match) => {
         let response = JSON.parse(body);
         let userResponse = response.msg;
         if (Array.isArray(response.msg)){
-          userResponse = `Here is the top picked heroes in the last month\n`;
+          userResponse = `Here is the top picked heroes in the last week\n`;
           response.msg.forEach((hero) => {
             userResponse += `\n${hero.hero} - ${hero.pick_rate}`;
           });
