4d04baf5259c8a10b6630fe2e7f6d3fecd54e001
leonardo a <leonardo.akato@gmail.com>
2017-12-29 02:13:31 -0200

    29 dezembro de 2017

--- server/bot/overwatch/registerUser.js
+++ server/bot/overwatch/registerUser.js
@@ -9,16 +9,23 @@ const request = require('request');
 const constants = require('../../utils/constants');
 
 module.exports = (msg, match) => {
-  console.log('entrou aqui');
-
   let chatId = msg.chat.id;
   let url = constants.url.overwatch.PREFIX + constants.url.overwatch.REGISTER_USER;
   let headers = constants.url.overwatch.HEADERS;
+  let body = {
+    id: msg.from.id,
+    battletag: "miake#12345"
+  };
 
-  request.post({url: url, headers: headers}, (err, httpResponse, body) => {
+  request.post({
+    url: url,
+    json: body,
+    headers: headers
+  }, (err, httpResponse, body) => {
     let response = JSON.parse(body);
-    console.log(response.msg);
+    console.log('resp msg: ');
     return bot.sendMessage(chatId, response.msg);
+    return bot.sendMessage(chatId, err);
   });
 
 };
