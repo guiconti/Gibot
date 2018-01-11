b2046c3c6dedebf642818ff495b45466b2a63af9
leonardo a <leonardo.akato@gmail.com>
2018-01-11 17:33:44 -0200

    added delete user regex

--- server/utils/constants.js
+++ server/utils/constants.js
@@ -41,6 +41,7 @@ module.exports = {
       || /\/overwatch_top_pick (.+)/i,
     CRYPTO_CURRENCY: /\/currency_converter/i || /\/currency_converter(.+)/i,
     CRYPTO_CURRENCY_CONVERTER: /currency_converter_(\S{3})_to_(\S{3})/i,
-    OVERWATCH_REGISTER_USER: /\/overwatch_register (.+)/i
+    OVERWATCH_REGISTER_USER: /\/overwatch_register (.+)/i,
+    OVERWATCH_DELETE_USER: /\/overwatch_delete (.+)/i
   }
 }
