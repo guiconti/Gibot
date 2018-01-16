7ab2ab7b1ca5ebf7f3e024161e86860bcddea050
leonardo a <leonardo.akato@gmail.com>
2018-01-16 15:01:55 -0200

    added coin full form caller

--- server/bot/cryptocurrency/commandCenter.js
+++ server/bot/cryptocurrency/commandCenter.js
@@ -5,7 +5,9 @@
 
 const constants = require('../../utils/constants');
 const getCryptoInfo = require('./convertCurrency');
+const getCoinFullForm = require('./getCoinFullForm');
 
 module.exports = bot => {
   bot.onText(constants.regex.CRYPTO_CURRENCY, getCryptoInfo);
+  bot.onText(constants.regex.CRYPTO_FULL_FORM, getCoinFullForm)
 };
